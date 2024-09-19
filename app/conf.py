72815c894a3157c4b45c322c6eaf941c1f27d3cb
Oren Cohen <oren@orencreates.com>
2024-09-19 14:02:08 +0300

    updating conf

--- app/conf.py
+++ app/conf.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    app_name: str = "Awesome API"
+    app_name: str = "Awesome Blog Backend API"
     items_per_user: int = 50
     is_dev: bool = True
     app_port: int = 8000
